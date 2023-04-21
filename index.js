1aab87e7e333af1cfee51e3ace292ba2c8ae94de
Sharonne24 <sawuor395@gmail.com>
2023-04-21 21:45:23 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+//Your Code Here
+    function submitData(name, email) {
+        const url = "http://localhost/3000/users";
+        const data = {
+          name: name,
+          email: email,
+        };
+      
+        const configurationObject = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(data),
+        };
+      
+        return fetch(url, configurationObject)
+          .then(resp => resp.json())
+          .then(submitData => {
+            console.log(submitData);
+            return submitData;
+          })
+          .catch(error => {
+            console.log(error.message);
+            return error.message;
+          });
+      }
+
+
+      
